753f79010c2520b4fc8a578e38ca1445d9bae34f
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-09-13 05:37:19 +0000

    [Start]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  devise_for :users
+  devise_for :users, only: [:show, :edit]
   root to: "homes#top"
   get '/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
